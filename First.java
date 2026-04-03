f9a93438c372d2b87b05f9cf4f4df1b8ef064b02
gauresh-245 <167985020+gauresh-245@users.noreply.github.com>
2026-04-03 13:39:30 +0530

    Add First.java to calculate the sum of two numbers

---
+++ First.java
@@ -0,0 +1,8 @@
+public class First{
+     public static void main(String[] args) {
+        int num1 = 195;
+        int num2 = 195;
+        int sum = num1 + num2;
+        System.out.println("The sum of " + num1 + " and " +num2 + " is: " + sum);
+   }
+}
